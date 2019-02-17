946c1cc0f7f6da1677ee55c56aeea6a19342dc2c
jkujku <j.kummerlo@gmail.com>
2019-02-17 22:32:13 +0100

    add sql script for db creation

--- sql/install-databases.sh
+++ sql/install-databases.sh
@@ -4,11 +4,14 @@ DB_PASSWORD=$2
 
 echo "`date -u` Setup databases"
 
-mysql --user=$DB_USER --password=$DB_PASSWORD -h localhost t3gdpr_typo3 < data/typo3_structure.sql
-echo "`date -u` SQL - Finished  data/dump_typo3_data.sql"
+mysql --user=$DB_USER --password=$DB_PASSWORD -h localhost t3gdpr_typo3 < data/typo3-databases.sql
+echo "`date -u` SQL - Finished  data/typo3-databases.sql"
 
-mysql --user=$DB_USER --password=$DB_PASSWORD -h localhost t3gdpr_typo3 < data/typo3_data.sql
-echo "`date -u` SQL - Finished  data/dump_typo3_data.sql"
+mysql --user=$DB_USER --password=$DB_PASSWORD -h localhost t3gdpr_typo3 < data/typo3-structure.sql
+echo "`date -u` SQL - Finished  data/typo3-structure.sql"
+
+mysql --user=$DB_USER --password=$DB_PASSWORD -h localhost t3gdpr_typo3 < data/typo3-data.sql
+echo "`date -u` SQL - Finished  data/typo3-data.sql"
 
 echo "`date -u` Setup databases finished"
 
